3ea0813121a7adc8a7917733761fe7e5111d0e71
varshanth-c <155641278+varshanth-c@users.noreply.github.com>
2024-06-15 11:43:45 +0530

    Add files via upload

---
+++ scripts1.js
@@ -0,0 +1,14 @@
+const 
+const selectOperator = (operator) ->{
+    if(x !=="")
+        {
+            y = x
+            x = ''
+            operator = operator
+            disableOperator();
+
+        }
+}
+const enableOperator = () => {
+    const operatoe_btn = document.querySelectorAll()
+}
